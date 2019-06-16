77dddfcf873b76be76e884d47f6baef0a72f942a
Bryce Hasshaw <bhasshaw@gmail.com>
2019-06-15 23:55:34 -0500

    added bootstrap

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let calculator = new Vue({
     el: '#calculator',
     data: {
-      bill: 0,
-      tipChoice: 0,
-      tipAmount: 0,
-      totalBill: 0
+      bill: null,
+      tipChoice: null,
+      tipAmount: null,
+      totalBill: null
     },
     methods: {
       calculateTip: function () {
